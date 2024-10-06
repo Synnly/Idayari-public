90820ce7f5bc6971b21f2a081972cebe5457fb09
Synnly <Emanuelfernandes54@gmail.com>
2024-10-06 17:49:10 +0200

    Déplacement de la declaration de la classe User et de la creation de la table de User dans son fichier.

--- src/database.js
+++ src/database.js
@@ -1,4 +1,5 @@
-import { Sequelize, DataTypes, Model } from "sequelize";
+import { Sequelize } from "sequelize";
+import User from "./model/User.js";
 
 const user = 'mysql';
 const host = "synnly.com:3306";
@@ -7,44 +8,12 @@ const dbname = "idayari";
 const dialect = "mysql"
 export const uri = `${dialect}://${user}:${pass}@${host}/${dbname}`;
 
-const sequelize = new Sequelize(uri);
+export const sequelize = new Sequelize(uri);
 
 await sequelize.authenticate(); // Si connexion impossible le script quitte ici
 
-class User extends Model {
-    // pour des méthodes/attributs (avec un nom différent des attributs définis en dessous)
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-	type: DataTypes.STRING,
-	unique: true,
-    },
-    email: {
-	type: DataTypes.STRING,
-	unique: true,
-    },
-    hashedPassword: {
-	type: DataTypes.TEXT,
-    },
-  },
-  // sequelize est un argument nécessaire
-  // "timestamps: false" indique de ne pas créer des attributs 'createdAt' et 'updatedAt'
-  // "tableName: "Users" indique le nom de la table dans la bdd
-  { sequelize, timestamps: false, tableName: "Users" },
-);
-
 // synchronise avec la bdd, crée la table si elle n'existe pas encore
+await User.initTable(sequelize);
 await User.sync();
 
-/**
- * ATTENTION, APRES CETTE LIGNE IL EST IMPÉRATIF DE RECRÉER UNE INSTANCE DE
- * SEQUELIZE SANS QUOI LES FONCTIONS ET REQUÊTES NE FONCTIONNERONT PAS
- */
-await sequelize.close();
+export default sequelize;
