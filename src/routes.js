7718b6bac1c1d0126382ff6bb58ef6fe10809b71
Outsider1010 <114062925+Outsider1010@users.noreply.github.com>
2024-10-13 14:49:26 +0200

    moved token functions in token.js and prevented the creation of an Agenda row if error

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,7 @@ import User from "./model/User.js";
 import Agenda from "./model/Agenda.js";
 import { sequelize } from "./database.js";
 import UserAgendaAccess from "./model/UserAgendaAccess.js";
-
-import dotenv from "dotenv"; //Import qui permet la manipulation des variables d'environnement
-import jwt from "jsonwebtoken";
-dotenv.config(); // Récupère et parse le fichier .env pour récupérer clé SECRET
+import { saveAuthentificationCookie } from "./token.js";
 
 export function index(req, res) {
   res.render("index");
@@ -45,19 +42,26 @@ export function creationAgendaGET(req, res) {
 }
 
 export async function creationAgendaPOST(req, res) {
+  let agenda = null;
     try {
-        const agenda = await Agenda.create({
-            nom: req.body.nom
-        });
-
-        await UserAgendaAccess.create({
+      agenda = await Agenda.create({
+          nom: req.body.nom
+      });
+    } catch (e) {
+      res.render("creerAgenda", {
+        errMsg: "Une erreur est inattendue survenue. Veuillez réessayer plus tard.",
+      });
+      return
+    }
+    try {
+      await UserAgendaAccess.create({
             idUser: res.locals.id,
             idAgenda: agenda.id,
             idOwner: res.locals.id,
         })
         res.redirect('/');
-    }
-    catch (e){
+    } catch (e){
+      await agenda.destroy();
       res.render("creerAgenda", {
         errMsg: "Une erreur inattendue est survenue. Veuillez réessayer plus tard.",
       });
@@ -94,37 +98,3 @@ export function deconnexion(req, res, next) {
   res.clearCookie("accessToken");
   res.redirect("/");
 }
-
-export function authenticate(req, res, next) {
-  try {
-    let token = req.cookies.accessToken; // ou alors req.cookies['accessToken'];
-    let user = jwt.verify(token, process.env.SECRET); //La fonction décripte le token
-    res.locals.user = user;
-    //IMPORTANT, à partir de maintenant, si res.locals.users est défini alors l'utilisateur est authentifié
-  } catch {}
-  next();
-}
-
-//A partir d'ici fonction pour l'authentification
-export function createJWT(user) {
-  return jwt.sign(
-    { id: user.id, username: user.username }, // données à crypter
-    process.env.SECRET, //Clé de chiffrement dans .env
-    { expiresIn: "1h" } //Durée de 1h
-  );
-}
-
-export function saveAuthentificationCookie(savedUser, res) {
-  let token = createJWT({ id: savedUser.id, username: savedUser.username }); //On crée le token représentant notre user
-  res.cookie("accessToken", token, { httpOnly: true });
-}
-
-/* explication sur l'authentification
-Lorsqu'on s'inscrit ou se connecte on enregistre le cookie accessToken qui représente l'objet User (en crypté)
-le middlewares   .use(routes.authenticate) va pour chaque requete, tester si le cookie accessToken contient un user, 
-si c'est le cas il va insérer l'user décripté dans les variables locales de la reponse actuelle (res.locals.user = user)
-
-Dans les différents templates, on a qu'a vérifier comme dans cette exemple : 
-<% if (locals.user) { %>
-    <p>Vous êtes connecté : <%= locals.user.username %></p>
-  */
