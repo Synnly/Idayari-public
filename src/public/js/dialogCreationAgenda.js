f018b2c541a604bc7372b00e600431b88aacd8fa
basualdo1u <lautaro.basualdo1@etu.univ-lorraine.fr>
2024-10-16 17:18:18 +0200

    dialog Creation Agenda (reset du nom agenda dans la boite dialog)

--- src/public/js/dialogCreationAgenda.js
+++ src/public/js/dialogCreationAgenda.js
@@ -3,6 +3,7 @@ const messageErreur = document.getElementById("messageErreur");
 const boiteDialog = document.getElementById("viewDialogCreationAgenda");
 const divDialog = document.getElementById("divDialogCreerAgenda");
 const boutonOuvreDialog = document.getElementById("ouvreDialogCreationAgenda");
+//const champNomAgendaDialog = document.getElementById("nomA");
 
 boutonOuvreDialog.addEventListener("click", ()=>{
     divDialog.hidden = false;
@@ -13,13 +14,20 @@ const boutonFermerDialog = document.getElementById("fermerDialogCreationAgenda")
 boutonFermerDialog.addEventListener("click", ()=>{
     boiteDialog.close();
     divDialog.hidden = true;
+    const champNom = document.getElementById("nom");
+    const nom = champNom.value.trim();
+    champNom.value = null;
 });
 
 const formulaireAgenda = document.getElementById("formulaireAgenda");
 formulaireAgenda.addEventListener("submit", async(e)=>{
     e.preventDefault();
     const dataFormulaire = new FormData(formulaireAgenda);
-    const nom = dataFormulaire.get("nom");
+    //const nom = dataFormulaire.get("nom");
+    const champNom = document.getElementById("nom");
+    const nom = champNom.value.trim();
+    champNom.value = null;
+    
 
     try {
         const x = await fetch("/creerAgenda",{
@@ -35,18 +43,15 @@ formulaireAgenda.addEventListener("submit", async(e)=>{
             /*boiteDialog.close();
             divDialog.hidden = true;
             window.location.reload();*/
-            // Mettre à jour l'interface utilisateur avec le nouvel agenda sans recharger
             const newAgendaItem = document.createElement('li');
             newAgendaItem.innerHTML = `<h3>${nom}</h3>`; // Créer un nouvel élément agenda
             document.querySelector('ul').appendChild(newAgendaItem); // Ajoute à la liste existante
 
-            // Fermer le dialog
             boiteDialog.close();
             divDialog.hidden = true;
         }else{
                 messageErreur.textContent = y.message;
         }
-
     } catch (error) {
         messageErreur.textContent = "erreur dans le catch du submit creation Agenda";
         console.error("Erreur :", error);
