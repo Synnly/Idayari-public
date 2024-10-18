791c2317e4d420cc45fbb2c592c4425d333e9d23
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2024-10-19 00:07:25 +0200

    ajout cookies notif

---
+++ src/public/js/index.js
@@ -0,0 +1,6 @@
+function cookies(accepted){
+    //Yes -> nothing to do !
+    //No -> nothing to do !
+    //Cookies are accepted in the two cases
+    console.table( {cookies : "cookies", accepted})
+}
