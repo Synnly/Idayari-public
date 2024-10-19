04a21aed2cbac633880ed6ca4e1c370652ede831
Outsider1010 <114062925+Outsider1010@users.noreply.github.com>
2024-10-19 05:07:27 +0200

    fichier script oublié

---
+++ src/public/js/script_rendezvous.js
@@ -0,0 +1,37 @@
+function change_recurrent_option(elem) {
+    document.getElementById('recurrent_div').style.display = elem.checked ? 'block' : 'none';
+    if (!elem.checked) {
+        reset(document.getElementById('freq_number'));
+        reset(document.getElementById('nb_occurence'));
+        reset(document.getElementById('date_fin_recurrence'));
+    } else {
+        document.getElementById('freq_number').required = true;
+    }
+}
+
+function change_fin_recurrence_option(elem) {
+    date_fin = document.getElementById('date_fin_recurrence');
+    nb_occur = document.getElementById('nb_occurence');
+    if (elem.value == "0") {
+        date_fin.style.display = 'block';
+        date_fin.required = true;
+        reset2(nb_occur);
+    } else if (elem.value == "1") {
+        nb_occur.style.display = 'block';
+        nb_occur.required = true;
+        reset2(date_fin);
+    } else {
+        reset2(nb_occur);
+        reset2(date_fin);
+    }
+}
+
+function reset(elem) {
+    elem.required = false;
+    elem.value = '';
+}
+
+function reset2(elem) {
+    reset(elem);
+    elem.style.display = 'none';
+}
