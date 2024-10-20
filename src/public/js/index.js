154a4ad0a617e3bc6067fa2433ab3cabf416cb5e
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2024-10-20 11:55:02 +0200

    suppression toast cookies

--- src/public/js/index.js
+++
@@ -1,6 +0,0 @@
-function cookies(accepted){
-    //Yes -> nothing to do !
-    //No -> nothing to do !
-    //Cookies are accepted in the two cases
-    console.table( {cookies : "cookies", accepted})
-}
