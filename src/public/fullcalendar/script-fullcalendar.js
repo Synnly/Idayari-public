bb87b62c663ae4dc1fd678160634eda37e7df2de
morad-aiachi-dashfile-at-394109254195 <aiachi3u@etu.univ-lorraine.fr>
2024-10-25 17:38:54 +0200

    Calendrier affiché sans lien avec le serveur

---
+++ src/public/fullcalendar/script-fullcalendar.js
@@ -0,0 +1,52 @@
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list';
+
+const elementCalendrier = document.getElementById('calendar');
+let evenements = [{"title": "Ceci est mon premier rdv","start": "2024-10-23 09:00:00","end": "2024-10-23 10:00:00"}]
+
+
+window.onload = () =>{ let calendrier = new Calendar(elementCalendrier,{
+    //Appel des différents composants composants
+    plugins : [dayGridPlugin,timeGridPlugin,listPlugin],
+    locale:'fr',
+    //Attention listWeek potentielleemnet à changer en list
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'} ,
+        buttonText: {
+            today:'Aujourd\'hui',
+            month:'Mois',
+            week:'Semaine',
+            list:'Liste',
+            day:"Jour"
+        },
+    //initialView: 'dayGridWeek'
+    // initialView: 'dayGridDay'
+    // initialView: 'dayGridMonth'
+    // initialView:'timeGridWeek'
+    // initialView:'timeGridDay'
+    // initialView:'list'
+     events:evenements
+
+
+
+});
+calendrier.render();
+}
+console.log('Hello motto');
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const calendarEl = document.getElementById('calendar')
+//     const calendar = new Calendar(calendarEl, {
+//       plugins: [dayGridPlugin],
+//       headerToolbar: {
+//         left: 'prev,next today',
+//         center: 'title',
+//         right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+//       }
+//     })
+//     calendar.render()
+//   })
