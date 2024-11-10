27e06fd7b9a82e3fc636059d10c8d5f133fa58a5
Outsider1010 <114062925+Outsider1010@users.noreply.github.com>
2024-11-10 23:59:08 +0100

    fix ajout des ecouteurs quand nouvel agenda

--- src/public/js/agenda.js
+++ src/public/js/agenda.js
@@ -8,6 +8,7 @@ const all_options_buttons = document.getElementsByClassName('dropbtn');
 const dialog_agenda = document.getElementById('dialogAgenda');
 const fermer_dialog = document.getElementById("fermerDialogAgenda");
 const sup_buttons = document.getElementsByClassName("sup-btn");
+const dropdowns = document.getElementsByClassName("dropdown");
 
 /**
  * Vérifie si tous les agendas sont sélectionnés
@@ -53,26 +54,28 @@ function close() {
  */
 fermer_dialog.addEventListener("click", close);
 
+
+function selectAgenda(agenda, event) {
+    const id = agenda.id.split("_")[1];
+    const checkbox = agenda.firstElementChild.firstElementChild;
+    if (event.target.tagName != "LABEL" && event.target.tagName != "INPUT") {
+        checkbox.checked = !checkbox.checked;
+    }
+    // si déselection, on sait que tout n'est plus sélectionné
+    if (!checkbox.checked) {
+        select_all.checked = false;
+        agendaManager.deselectionAgenda(id);
+    } else {
+        // sinon, on vérifie si tout est sélectionné
+        select_all.checked = every_agenda_selected();
+        agendaManager.selectionAgenda(id);
+    }
+}
 /**
  * Click sur un agenda
  */
 for(const agenda of list_agendas.children) {
-    agenda.addEventListener('click', (event) => {
-        const id = agenda.id.split("_")[1];
-        const checkbox = agenda.firstElementChild.firstElementChild;
-        if (event.target.tagName != "LABEL" && event.target.tagName != "INPUT") {
-            checkbox.checked = !checkbox.checked;
-        }
-        // si déselection, on sait que tout n'est plus sélectionné
-        if (!checkbox.checked) {
-            select_all.checked = false;
-            agendaManager.deselectionAgenda(id);
-        } else {
-            // sinon, on vérifie si tout est sélectionné
-            select_all.checked = every_agenda_selected();
-            agendaManager.selectionAgenda(id);
-        }
-    });
+    agenda.addEventListener('click', (event) => selectAgenda(agenda, event));
 }
 
 function clearOptionsDIV() {
@@ -85,17 +88,20 @@ function clearOptionsDIV() {
     }
 }
 
+// factorise le fait d'empecher la sélection d'agendas quand on clique sur les boutons du menu
+// ou les 3 points
+for (const elem of dropdowns) {
+    elem.addEventListener('click', (event) => event.stopPropagation());
+}
+
+function openDropDownMenu(elem) {
+    elem.nextElementSibling.classList.toggle('noshow');
+}
 /**
  * Click sur les 3 points
  */
 for (const elem of all_options_buttons) {
-    elem.addEventListener('click', (event) => {
-        // empeche de sélectionner l'agenda
-        event.stopPropagation();
-        // supprimer les anciens menus
-        clearOptionsDIV();
-        elem.nextElementSibling.classList.toggle('noshow');
-    });
+    elem.addEventListener('click', () => openDropDownMenu(elem));
 }
 /**
  * Supprime le menu quand on clique en dehors
@@ -106,34 +112,34 @@ window.addEventListener('click', (event) => {
     }
 });
 
+function supprimerAgenda(sup_button) {
+    const id = sup_button.getAttribute('data-id');
+    const nom = sup_button.getAttribute('data-nom');
+    if(confirm(`Êtes vous sûr de supprimer l'agenda ${nom}\nCette action est IRREVERSIBLE.`)){
+        fetch(`/supprimerAgenda/${id}`, { method: "DELETE" })
+        .then(res => {
+            // l'agenda a été trouvé avec succès et supprimé
+            if (res.status === 202) {
+                const removed_agenda = document.getElementById(`agenda_${id}`);
+                const was_selected = removed_agenda.firstElementChild.firstElementChild.checked;
+                removed_agenda.remove();
+                if (!was_selected) {
+                    // Il est possible que la suppression de l'agenda fasse que tous les élements soient sélectionnés
+                    select_all.checked = every_agenda_selected();
+                }
+                agendaManager.removeAgenda(id, was_selected);
+            }
+        })
+        .catch(error => {
+            console.log(`erreur suppression agenda: ${error}`);
+        })
+    }
+}
 /**
  * Suppression d'un agenda
  */
 for (const sup_button of sup_buttons) {
-    sup_button.addEventListener('click', (event) => {
-        event.stopPropagation();
-        const id = sup_button.getAttribute('data-id');
-        const nom = sup_button.getAttribute('data-nom');
-        if(confirm(`Êtes vous sûr de supprimer l'agenda ${nom}\nCette action est IRREVERSIBLE.`)){
-            fetch(`/supprimerAgenda/${id}`, { method: "DELETE" })
-            .then(res => {
-                // l'agenda a été trouvé avec succès et supprimé
-                if (res.status === 202) {
-                    const removed_agenda = document.getElementById(`agenda_${id}`);
-                    const was_selected = removed_agenda.firstElementChild.firstElementChild.checked;
-                    removed_agenda.remove();
-                    if (!was_selected) {
-                        // Il est possible que la suppression de l'agenda fasse que tous les élements soient sélectionnés
-                        select_all.checked = every_agenda_selected();
-                    }
-                    agendaManager.removeAgenda(id, was_selected);
-                }
-            })
-            .catch(error => {
-                console.log(`erreur suppression agenda: ${error}`);
-            })
-        }
-    });
+    sup_button.addEventListener('click', () => supprimerAgenda(sup_button));
 }
 
 const form = document.getElementById("agenda_form");
@@ -150,12 +156,27 @@ form.addEventListener("submit", (event) => {
     })
     .then(result => {
         list_agendas.insertAdjacentHTML('beforeend', result.html);
+
+        // ajout des écouteurs (NE PAS OUBLIER)
+        const agenda = document.getElementById(`agenda_${result.data.id}`);
+        agenda.addEventListener('click', (event) => selectAgenda(agenda, event));
+        const dropdown = agenda.lastElementChild;
+        dropdown.addEventListener('click', (event) => event.stopPropagation());
+        const option = dropdown.firstElementChild;
+        option.addEventListener('click', () => openDropDownMenu(option));
+        // les options (modifier, supprimer, etc...)
+        for (const elem of option.nextElementSibling.children) {
+            if (elem.classList.contains('sup-btn')) {
+                elem.addEventListener('click', () => supprimerAgenda(elem));
+            }
+            // IMPORTANT : Une fois le reste géré, NE PAS OUBLIER D'AJOUTER LES ECOUTEURS SI NOUVEL AGENDA
+        }
+
         agendaManager.addAgenda(result.data);
         // si le bouton "tout selectionner" était activé (donc tout était sélectionné)
         // et qu'on rajoute un agenda non sélectionné, on le désélectionne
-        const all_select_button = document.getElementById('selectAll');
-        if (all_select_button.checked && !result.data.agenda.displayed) {
-            all_select_button.checked = false;
+        if (select_all.checked && !result.data.agenda.displayed) {
+            select_all.checked = false;
         }
         close();
     })
