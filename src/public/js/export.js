7da4a894dc965f16d9e60f355b821678546fc77f
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2024-11-12 20:23:02 +0100

    FIX erreur avec les noms des agendas

--- src/public/js/export.js
+++ src/public/js/export.js
@@ -1,4 +1,5 @@
 function selectAgenda(id, nom) {
+    console.log(nom + "     1")
     const agendaItems = document.querySelectorAll('#agendaList .list-group-item');
     agendaItems.forEach(item => item.classList.remove('bg-primary'));
     document.getElementById(`${id}`).classList.add('bg-primary');
@@ -9,6 +10,7 @@ function selectAgenda(id, nom) {
 }
 
 function exportAgenda(nom, agendaId) {
+    console.log(nom + "     2")
     document.getElementById("spinner").classList.remove("d-none");
     document.getElementById("spinner").classList.add("d-flex");
     fetch('/export', {
@@ -20,13 +22,18 @@ function exportAgenda(nom, agendaId) {
     })
     .then(response => response.json())
     .then(data => {
-        download(`${nom}.json`, JSON.stringify(data, null, 2));
-        window.location.reload();
+        download(`${decodeURIComponent(nom)}.json`, JSON.stringify(data, null, 2));
+        
     })
-    .catch(error => console.error('Erreur lors de l\'exportation de l\'agenda:', error));
+    .catch(error => console.error('Erreur lors de l\'exportation de l\'agenda:', error)).finally(() => {
+        document.getElementById("spinner").classList.remove("d-flex");
+        document.getElementById("spinner").classList.add("d-none");
+    }
+    );
 }
 
 function download(filename, text) {
+    console.log(filename + "     3")
     const blob = new Blob([text], {type: 'application/json;charset=utf-8'});
     const url = URL.createObjectURL(blob);
     const element = document.createElement('a');
