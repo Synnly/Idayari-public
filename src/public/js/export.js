640f09fbd93204b2fe6bd063b3d5dc433bc2af72
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2024-11-18 22:33:38 +0100

    appel export agenda sur la page d'accueil

--- src/public/js/export.js
+++ src/public/js/export.js
@@ -1,13 +1,3 @@
-function selectAgenda(id, nom) {
-    const agendaItems = document.querySelectorAll('#agendaList .list-group-item');
-    agendaItems.forEach(item => item.classList.remove('bg-primary'));
-    document.getElementById(`${id}`).classList.add('bg-primary');
-
-    const exportBtn = document.getElementById('exportBtn');
-    exportBtn.classList.remove('d-none');
-    exportBtn.setAttribute('onclick', `exportAgenda("${nom}", "${id}")`);
-}
-
 function exportAgenda(nom, agendaId) {
     document.getElementById("spinner").classList.remove("d-none");
     document.getElementById("spinner").classList.add("d-flex");
@@ -29,15 +19,18 @@ function exportAgenda(nom, agendaId) {
     }
     );
 }
-
 function download(filename, text) {
-    const blob = new Blob([text], {type: 'application/json;charset=utf-8'});
+    const blob = new Blob([text], { type: 'application/json;charset=utf-8' });
     const url = URL.createObjectURL(blob);
+
     const element = document.createElement('a');
     element.href = url;
     element.download = filename;
+
     document.body.appendChild(element);
     element.click();
+
     document.body.removeChild(element);
+
     URL.revokeObjectURL(url);
 }
